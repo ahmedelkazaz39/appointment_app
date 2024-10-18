6759403740cd5cbb9615691012de85c8a860da30
Ahmed Mohamed Elkazaz <112720975+ahmedelkazaz39@users.noreply.github.com>
2024-10-18 22:26:59 +0300

    add more svg pictures and finish docdoc logo and name

---
+++ lib/core/theme/image_manage.dart
@@ -0,0 +1,6 @@
+class ImageManager {
+  static const String logoImage = 'assets/svgs/logo.svg';
+  static const String docDocImage = 'assets/svgs/Docdoc.svg';
+  static const String docDocLogoLowOpacty =
+      'assets/svgs/doc_docO_logo_low_obacity.svg';
+}
--- lib/features/onboarding/onboarding_screen.dart
+++ lib/features/onboarding/onboarding_screen.dart
@@ -1,13 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+import 'widgets/doc_logo_and_name.dart';
 
 class OnboardingScreen extends StatelessWidget {
   const OnboardingScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-        body: Center(
-      child: Text('ahmed'),
+    return Scaffold(
+        body: SafeArea(
+      child: SingleChildScrollView(
+        child: Padding(
+          padding: EdgeInsets.symmetric(
+            vertical: 30.h,
+          ),
+          child: const Column(
+            children: [
+              DocLogoAndName(),
+            ],
+          ),
+        ),
+      ),
     ));
   }
 }
